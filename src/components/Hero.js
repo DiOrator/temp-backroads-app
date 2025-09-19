35a2ed1d7b67f148778c869288a3d4ceb3da6a8a
DiOrator <oratorsconnect.gmail.com>
2025-09-19 20:04:44 +0100

    yeah

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,13 +2,14 @@ const Hero = () => {
   return (
     <section className='hero' id='home'>
       <div className='hero-banner'>
-        <h1>From solomon to blessing</h1>
+        <h1>Backroads App</h1>
         <p>
-          Actually, I hope you know who I am from our conversations, also that I
-          am more than you know. I'm doing this through coding, lol
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae
+          tempore voluptatum maxime reprehenderit eum quod exercitationem fugit,
+          qui corporis
         </p>
         <a href='#tours' className='btn hero-btn '>
-          She said buy perfume
+          Explore Tours
         </a>
       </div>
     </section>
